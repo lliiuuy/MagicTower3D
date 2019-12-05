dd94036a4475feda0aa4ab9816c849c4043fb69f
lliiuuy <1351437740@qq.com>
2019-12-05 14:01:43 +0800

    build successfully

--- BlueMedicine.cpp
+++ BlueMedicine.cpp
@@ -3,6 +3,5 @@
 BlueMedicine::BlueMedicine(Vector2* positionInMap): ConsumbleItem(positionInMap)
 {
 	this->health = 200;
-	this->tag = Tag::blueMedicine;
 	this->name = "BlueMedicine";
 }
--- RedMedicine.cpp
+++ RedMedicine.cpp
@@ -3,6 +3,5 @@
 RedMedicine::RedMedicine(Vector2* positionInMap): ConsumbleItem(positionInMap)
 {
 	this->health = 50;
-	this->tag = Tag::redMedicine;
 	this->name = "RedMedicine";
 }
