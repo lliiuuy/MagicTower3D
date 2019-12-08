f53bc95d08a5d2a485c71ef0309300568fc82393
lliiuuy <1351437740@qq.com>
2019-12-08 21:45:51 +0800

    Finish class Player FrameWork

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,47 @@
 #include "Player.h"
+
+void Player::move(Vector2* direction)
+{
+}
+
+void Player::reciveItems(ConsumbleItem* consumbleItem)
+{
+	if (consumbleItem->getTag() == Tag::sword)
+		this->sword = (Sword*)consumbleItem;
+	else if (consumbleItem->getTag() == Tag::shield)
+		this->shield = (Shield*)consumbleItem;
+	// 将消耗物品的属性加到人物身上
+	this->health += consumbleItem->getHealth();
+	this->attack += consumbleItem->getAttack();
+	this->defence += consumbleItem->getDefence();
+	this->yellowKeyNumber += consumbleItem->getYellowKeyNumber();
+	this->blueKeyNumber += consumbleItem->getBlueKeyNumber();
+	this->redKeyNumber += consumbleItem->getRedKeyNumber();
+
+	// 更新UI界面
+	// UIManager.update();
+}
+
+void Player::reciveUseItems(UseItem* useItem)
+{
+	// 将UI界面上的UseItem图标激活
+	// unsigned short index = useItem->get
+	// UIManager.enableUseItem(index)
+	// UIManager.update();
+	// 将物体插入指定位置
+	// useItems->insert(index, useItem);
+}
+
+Player::Player(Vector2* positionInMap): Creature(positionInMap)
+{
+	yellowKeyNumber = 0;
+	blueKeyNumber = 0;
+	redKeyNumber = 0;
+	moveSpeed = 0.0f;
+	positionNow = position;
+	shield = NULL;
+	sword = NULL;
+	spin = new Vector3(0, 0, 0);
+	spinSpeed = 0.0f;
+	useItems = new std::vector<UseItem*>();
+}
--- Player.h
+++ Player.h
@@ -1,7 +1,43 @@
 #pragma once
 #include "Creature.h"
+#include "Sword.h"
+#include "Shield.h"
+#include "UseItem.h"
+
 class Player :
 	public Creature
 {
+private:
+	unsigned short yellowKeyNumber; // 黄钥匙的数量
+	unsigned short blueKeyNumber; // 蓝钥匙的数量
+	unsigned short redKeyNumber; // 红钥匙的数量
+
+	Sword* sword; // 玩家所持有的剑
+	Shield* shield; // 玩家持有的盾
+
+	Vector3* spin; // 玩家的朝向
+	float spinSpeed; // 玩家的转向速度
+
+	Vector3* positionNow; // 玩家现在所处的位置
+	float moveSpeed; // 玩家的移动速度
+
+	std::vector<UseItem*> *useItems; // 玩家的使用型物品列表
+
+public:
+	void move(Vector2* direction);
+	// void spin(bool isLeft); 这边还在考虑要不要自由转动视角
+
+	unsigned short getYellowKeyNumber() { return yellowKeyNumber; }
+	unsigned short getBlueKeyNumber() { return blueKeyNumber; }
+	unsigned short getRedKeyNumber() { return redKeyNumber; }
+
+	Sword* getSword() { return sword; }
+	Shield* getShield() { return shield; }
+
+	void reciveItems(ConsumbleItem* consumbleItem); // 获取消耗物品
+	void reciveUseItems(UseItem* useItem); // 获取使用型物体
+
+	Player(Vector2* positionInMap);
+
 };
 
